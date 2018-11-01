d3afffa832a7435965aafd3dfaefb78fb07df329
Malik Alwan <malwan1@css.edu>
2018-11-01 10:55:22 -0500

    405 log message

--- SingleResponsibilityPrinciple/TradeProcessor.cs
+++ SingleResponsibilityPrinciple/TradeProcessor.cs
@@ -75,12 +75,28 @@ namespace SingleResponsibilityPrinciple
                 return false;
             }
 
+            int.TryParse(fields[1], out tradeAmount);
+                if ((tradeAmount < 1000) || (tradeAmount > 100000))  {
+                LogMessage("WARN", " Trade amount on line {0} not a valid integer: '{1}'", currentLine, fields[1]);
+                return false;
+
+            }
+
+            
+
             return true;
         }
 
-        private void LogMessage(string msgType, string message, params object[] args)
+        private void LogMessage(string type, string message, params object[] args)
         {
-            Console.WriteLine(msgType+ " :" +message, args);
+           
+            {
+                Console.WriteLine(type + ": " + message, args);
+                using (StreamWriter logfile = File.AppendText("log.xml"))
+                {
+                    logfile.WriteLine("<log><type>" + type + "</type><message>" + message + "</message></log> ", args);
+                }
+            }
         }
 
         private TradeRecord MapTradeDataToTradeRecord(string[] fields)
@@ -104,7 +120,8 @@ namespace SingleResponsibilityPrinciple
         private void StoreTrades(IEnumerable<TradeRecord> trades)
         {
             LogMessage("INFO", "  Connecting to Database");
-            using (var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30;"))
+             string connectSqlServer = "Data Source = athena.css.edu; Initial Catalog = CIS3285; Persist Security Info = True; User ID = tgibbons; Password = Data Source = athena.css.edu; Initial Catalog = CIS3285; Persist Security Info = True; User ID = tgibbons; Password = Saints4CSS";
+            using (var connection = new System.Data.SqlClient.SqlConnection(connectSqlServer))
             {
                 connection.Open();
                 using (var transaction = connection.BeginTransaction())
@@ -138,6 +155,6 @@ namespace SingleResponsibilityPrinciple
             StoreTrades(trades);
         }
 
-
+        public void 
     }
 }
